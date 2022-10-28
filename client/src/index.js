f035c265c10a0b04fa0b6a9844bf77a2999a9a86
nimishm2 <nimishm2@illinois.edu>
2022-10-28 13:20:27 -0500

    slider finally working

--- client/src/index.js
+++ client/src/index.js
@@ -9,10 +9,12 @@ import "./maps/styles.css";
 // React Elements
 import App from "./app/App";
 import CountryMap from "./maps/CountryMap";
+import DiscreteSlider from "./maps/slider";
 import DropDown from "./buttons/dropdown";
 
 const rootElement = document.getElementById("root");
-ReactDOM.render(<><App /><DropDown /><CountryMap /></>, rootElement);
+ReactDOM.render(<><App /><DropDown /><CountryMap /><DiscreteSlider/></>, rootElement);
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
