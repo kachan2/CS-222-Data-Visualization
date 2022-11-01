cac3ae0b3efd371939345079e7b18d355f02837d
Kaitlyn Chan <kachan2@illinois.edu>
2022-11-01 10:06:48 -0500

    adding data to map

--- client/src/maps/CountryMap.js
+++ client/src/maps/CountryMap.js
@@ -1,33 +1,32 @@
-// import React, { useState, useEffect } from "react";
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { ComposableMap, Geographies, Geography } from "react-simple-maps";
-// import { scaleQuantile } from "d3-scale";
-// import { csv } from "d3-fetch";
+import { scaleQuantile } from "d3-scale";
+import { csv } from "d3-fetch";
 
 const geoUrl = "https://cdn.jsdelivr.net/npm/us-atlas@3/counties-10m.json";
 
 const CountryMap = () => {
-  // const [data, setData] = useState([]);
+  const [data, setData] = useState([]);
 
-  // useEffect(() => {
-  //   csv("/data/f22_clean.csv").then(counties => {
-  //     setData(counties);
-  //   });
-  // }, []);
+  useEffect(() => {
+    csv("/data/f22_clean.csv").then(counties => {
+      setData(counties);
+    });
+  }, []);
 
-  // const colorScale = scaleQuantile()
-  //   .domain(data.map(d => d.population))
-  //   .range([
-  //     "#ffedea",
-  //     "#ffcec5",
-  //     "#ffad9f",
-  //     "#ff8a75",
-  //     "#ff5533",
-  //     "#e2492d",
-  //     "#be3d26",
-  //     "#9a311f",
-  //     "#782618"
-  //   ]);
+  const colorScale = scaleQuantile()
+    .domain(data.map(d => d.population))
+    .range([
+      "#ffedea",
+      "#ffcec5",
+      "#ffad9f",
+      "#ff8a75",
+      "#ff5533",
+      "#e2492d",
+      "#be3d26",
+      "#9a311f",
+      "#782618"
+    ]);
 
   return (
     <div>
@@ -35,12 +34,12 @@ const CountryMap = () => {
       <Geographies geography={geoUrl}>
         {({ geographies }) =>
           geographies.map(geo => {
-            // const cur = data.find(s => s.id === geo.id);
+            const cur = data.find(s => s.id === geo.id);
             return (
               <Geography
                 key={geo.rsmKey}
                 geography={geo}
-                // fill={cur ? colorScale(cur.population) : "#EEE"}
+                fill={cur ? colorScale(cur.population) : "#EEE"}
               />
             );
           })
